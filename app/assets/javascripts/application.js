206329f5a2cf1aaf8c82d74a8fbe40c075101064
Ken T <kentoler@gmail.com>
2014-09-17 15:54:31 -0400

    fonts working

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,28 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery-1.8.3.min
+//= require jquery-ui-1.9.2.custom.min
+//= require jquery.backstretch.min
+//= require jquery.dcjqaccordion.2.7
+//= require jquery.nicescroll
+//= require jquery.scrollTo.min
+//= require jquery.sparkline
+//= require jquery.tagsinput
+//= require jquery.ui.touch-punch.min
+//= require morris-conf
+//= require sparkline-chart
+//= require tasks
+//= require zabuto_calendar
+//= require bootstrap.min
+//= require bootstrap-switch
